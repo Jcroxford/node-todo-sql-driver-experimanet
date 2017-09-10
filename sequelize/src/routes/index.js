5fe6b27e6c39d1d7de0fdd5989c66db92be0ea53
Jcroxford <takeflightsc@gmail.com>
2017-09-09 18:05:05 -0600

    added route to create a todoItem
    /api/todos/create/:todosId/item post route was added. it takes content as a string value in its json and adds it to todoItems useing the todosId passed in the url to determin the todos list it should be associated with.

--- sequelize/src/routes/index.js
+++ sequelize/src/routes/index.js
@@ -1,6 +1,12 @@
 const todosController = require('../controllers').todos
+const todoItemsController = require('../controllers').todoItems
 
 module.exports = (app) => {
+  
+  // todos routes
   app.post('/api/todos/create', todosController.create)
   app.get('/api/todos/list', todosController.list)
+
+  // todo items routes
+  app.post('/api/todos/create/:todoId/items', todoItemsController.create)
 }
