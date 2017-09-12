9764e0d2bd6fb24159c943940da871efa9c3ce8d
Jcroxford <takeflightsc@gmail.com>
2017-09-12 14:05:17 -0600

    refactored getTodos and added single todo retrieval
    refactored code used to format todos into their own functions. These functions are used in the single and multi todo retrievals.

--- knex/src/db/queries.js
+++ knex/src/db/queries.js
@@ -1,6 +1,40 @@
 const knex = require('./knex')
 const _ = require('lodash')
 
+// ===========================
+// helper methods
+// ===========================
+// formats todos to be similar to that of sequelize reponse
+// this is based on migration 20170911143445_TodoItems & 20170911142255_Todos
+const formatTodos = (todos) => {
+  const formattedTodos = []
+  
+  _.forIn(todos, (value, key) => {
+    const { id, title, createdAt, updatedAt } = value[0]
+    const todo = { id, title, createdAt, updatedAt }
+
+    todo.todoItems = []
+    for(todoItem of value) {
+      const item = {
+        id: todoItem['item.id'],
+        content: todoItem['item.content'],
+        complete: todoItem['item.complete'],
+        createdAt: todoItem['item.createdAt'],
+        updatedAt: todoItem['item.updatedAt'],
+        todoId: todoItem['item.todoId']
+      }
+
+      todo.todoItems.push(item)
+    }
+
+    formattedTodos.push(todo)
+  })
+
+  return formattedTodos
+}
+
+const groupTodosByTodoId = todos => _.groupBy(todos, 'item.todoId')
+
 module.exports = {
 
   // ===========================
@@ -21,48 +55,47 @@ module.exports = {
           'Todos.title',
           'Todos.createdAt',
           'Todos.updatedAt',
-          'TodoItems.id as todoItems.id',
-          'TodoItems.content as todoItems.content',
-          'TodoItems.complete as todoItems.complete',
-          'TodoItems.createdAt as todoItems.createdAt',
-          'TodoItems.updatedAt as todoItems.updatedAt',
-          'TodoItems.todoId as todoItems.todoId'
+          'TodoItems.id as item.id',
+          'TodoItems.content as item.content',
+          'TodoItems.complete as item.complete',
+          'TodoItems.createdAt as item.createdAt',
+          'TodoItems.updatedAt as item.updatedAt',
+          'TodoItems.todoId as item.todoId'
         )
         .from('Todos')
         .leftJoin('TodoItems', 'Todos.id', 'TodoItems.todoId')
-        .then(todos => _.groupBy(todos, 'todoItems.todoId'))
-        .then(todos => {
-          const formattedTodos = []
-  
-          _.forIn(todos, (value, key) => {
-            const { id, title, createdAt, updatedAt } = value[0]
-            const todo = { id, title, createdAt, updatedAt }
-  
-            todo.todoItems = []
-            for(todoItem of value) {
-              const item = {
-                id: todoItem['todoItems.id'],
-                content: todoItem['todoItems.content'],
-                complete: todoItem['todoItems.complete'],
-                createdAt: todoItem['todoItems.createdAt'],
-                updatedAt: todoItem['todoItems.updatedAt'],
-                todoId: todoItem['todoItems.todoId']
-              }
-  
-              todo.todoItems.push(item)
-            }
-  
-            formattedTodos.push(todo)
-          })
-  
-          resolve(formattedTodos)
-        })
+        .then(todos => groupTodosByTodoId(todos))
+        .then(todos => resolve(formatTodos(todos)))
+        .catch(error => reject(error))
+    })
+  },
+
+  getTodoById(todoId) {
+    return new Promise((resolve, reject) => {
+      knex
+        .select(
+          'Todos.id',
+          'Todos.title',
+          'Todos.createdAt',
+          'Todos.updatedAt',
+          'TodoItems.id as item.id',
+          'TodoItems.content as item.content',
+          'TodoItems.complete as item.complete',
+          'TodoItems.createdAt as item.createdAt',
+          'TodoItems.updatedAt as item.updatedAt',
+          'TodoItems.todoId as item.todoId'
+        )
+        .from('Todos')
+        .leftJoin('TodoItems', 'Todos.id', 'TodoItems.todoId')
+        .where('Todos.id', todoId)
+        .then(todos => groupTodosByTodoId(todos))
+        .then(todos => resolve(formatTodos(todos)[0])) // removing from array because there should only ever be 1 object
         .catch(error => reject(error))
     })
   },
 
   // ===========================
-  // todo methods
+  // todoItem methods
   // ===========================
   createTodoItem(content, todoId) {
     return knex
