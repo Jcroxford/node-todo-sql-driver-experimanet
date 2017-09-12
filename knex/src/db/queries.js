6f6e63010e32158135352f5cb5cd31f2fb617597
Jcroxford <takeflightsc@gmail.com>
2017-09-11 23:31:52 -0600

    updated todos list route to also associated todoItems
    updated query and used lodash to mimick the formatting receive from sequelize

--- knex/src/db/queries.js
+++ knex/src/db/queries.js
@@ -1,4 +1,5 @@
 const knex = require('./knex')
+const _ = require('lodash')
 
 module.exports = {
 
@@ -13,9 +14,51 @@ module.exports = {
   },
 
   getAllTodos() {
-    return knex
-      .select()
-      .from('Todos')
+    return new Promise((resolve, reject) => {
+      knex
+        .select(
+          'Todos.id',
+          'Todos.title',
+          'Todos.createdAt',
+          'Todos.updatedAt',
+          'TodoItems.id as todoItems.id',
+          'TodoItems.content as todoItems.content',
+          'TodoItems.complete as todoItems.complete',
+          'TodoItems.createdAt as todoItems.createdAt',
+          'TodoItems.updatedAt as todoItems.updatedAt',
+          'TodoItems.todoId as todoItems.todoId'
+        )
+        .from('Todos')
+        .leftJoin('TodoItems', 'Todos.id', 'TodoItems.todoId')
+        .then(todos => _.groupBy(todos, 'todoItems.todoId'))
+        .then(todos => {
+          const formattedTodos = []
+  
+          _.forIn(todos, (value, key) => {
+            const { id, title, createdAt, updatedAt } = value[0]
+            const todo = { id, title, createdAt, updatedAt }
+  
+            todo.todoItems = []
+            for(todoItem of value) {
+              const item = {
+                id: todoItem['todoItems.id'],
+                content: todoItem['todoItems.content'],
+                complete: todoItem['todoItems.complete'],
+                createdAt: todoItem['todoItems.createdAt'],
+                updatedAt: todoItem['todoItems.updatedAt'],
+                todoId: todoItem['todoItems.todoId']
+              }
+  
+              todo.todoItems.push(item)
+            }
+  
+            formattedTodos.push(todo)
+          })
+  
+          resolve(formattedTodos)
+        })
+        .catch(error => reject(error))
+    })
   },
 
   // ===========================
