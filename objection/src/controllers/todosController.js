e1983fca3c2134d8a94ebf8fbfcf5f19266a748e
Jcroxford <takeflightsc@gmail.com>
2017-09-14 16:39:59 -0600

    added initial logic for displaying all todo lists
    added get route /api/todos/list which lists all todo lists but not their items yet

--- objection/src/controllers/todosController.js
+++ objection/src/controllers/todosController.js
@@ -9,6 +9,13 @@ module.exports = {
       .then(todo => Todos.query().where('id', '=', todo.id))
       .then(todos => res.status(201).send(todos[0]))
       .catch(error => res.status(400).send(error))
+  },
+
+  list(req, res) {
+    Todos
+      .query()
+      .then(todos => res.send(todos))
+      .catch(error => res.status(400).send(error))
   }
 
 }
--- objection/src/routes/index.js
+++ objection/src/routes/index.js
@@ -2,6 +2,11 @@ const todosController =  require('../controllers').todosController
 
 module.exports = (app) => {
   
+  // todos routes
   app.post('/api/todos/create', todosController.create)
+  app.get('/api/todos/list', todosController.list)
+
+  // todo items routes
+
 
 }
